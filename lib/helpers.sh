8336e0c543516453987d09c796f09332b4a68131
Chris Rose <offline@offby1.net>
2022-07-13 07:20:59 -0700

    bug: fix the file finding on linux
    I lifted this wholesale from the amazing "json parsing" that the jq
    plugin does.

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -9,12 +9,12 @@ get_platform() {
 	local platform_check=${ASDF_GAM_OVERWRITE_PLATFORM:-"$platform"}
 
 	case "$platform_check" in
-	linux | darwin)
-		[ -z "$silent" ] && msg "Platform '${platform_check}' supported!"
-		;;
-	*)
-		fail "Platform '${platform_check}' not supported!"
-		;;
+		linux | darwin)
+			[ -z "$silent" ] && msg "Platform '${platform_check}' supported!"
+			;;
+		*)
+			fail "Platform '${platform_check}' not supported!"
+			;;
 	esac
 
 	echo -n "$platform_check"
@@ -25,8 +25,8 @@ get_platform_for_download() {
 	platform=$(get_platform silently)
 
 	case "$platform" in
-	darwin) echo -n "macos" ;;
-	*) echo -n "$platform" ;;
+		darwin) echo -n "macos" ;;
+		*) echo -n "$platform" ;;
 	esac
 }
 
@@ -37,21 +37,21 @@ get_arch() {
 	platform=$(get_platform silently)
 	local platform_arch="${arch_check}:${platform}"
 	case "${platform_arch}" in
-	*:darwin)
-		arch="universal2"
-		;;
-	x86_64:* | amd64:*)
-		arch="x86_64"
-		;;
-	armv7l:*)
-		arch="armv7l"
-		;;
-	aarch64:* | arm64:*)
-		arch="aarch64"
-		;;
-	*)
-		fail "Arch '${arch_check}' not supported!"
-		;;
+		*:darwin)
+			arch="universal2"
+			;;
+		x86_64:* | amd64:*)
+			arch="x86_64"
+			;;
+		armv7l:*)
+			arch="armv7l"
+			;;
+		aarch64:* | arm64:*)
+			arch="aarch64"
+			;;
+		*)
+			fail "Arch '${arch_check}' not supported!"
+			;;
 	esac
 
 	echo -n $arch
@@ -65,21 +65,21 @@ get_suffix() {
 	platform=$(get_platform silently)
 	local platform_arch="${arch}:${platform}"
 	case "${platform_arch}" in
-	*:darwin)
-		suffix=""
-		;;
-	aarch64:*)
-		suffix="-glibc2.28"
-		;;
-	armv7l:*)
-		suffix="-glibc2.28"
-		;;
-	x86_64:*)
-		suffix="-glibc2.31"
-		;;
-	*)
-		fail "Platform/Arch '${platform_arch}' not supported!"
-		;;
+		*:darwin)
+			suffix=""
+			;;
+		aarch64:*)
+			suffix="-glibc2.28"
+			;;
+		armv7l:*)
+			suffix="-glibc2.28"
+			;;
+		x86_64:*)
+			suffix="-glibc2.31"
+			;;
+		*)
+			fail "Platform/Arch '${platform_arch}' not supported!"
+			;;
 	esac
 
 	echo -n $suffix
@@ -99,6 +99,11 @@ msg() {
 
 err() {
 	echo -e "\033[31m$1\033[39m" >&2
+}
+
+error_exit() {
+	err "$1"
+	exit "${2:-1}"
 }
 
 fail() {
